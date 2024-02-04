c04e8e606c8fcfc9b9d05881966128b36ee2245e
Shruti Shinde <139608230+shrutishinde11@users.noreply.github.com>
2024-02-04 15:41:36 +0530

    Add files via upload
    *
    **
    ***
    ****
    *****
    ****
    ***
    **
    *

---
+++ pattern3.py
@@ -0,0 +1,20 @@
+row = 5
+for i in range(0,6):
+    for j in range (i):
+        print("*", end="")
+    print("")
+for k in range(4,0,-1):
+    for l in range (k):
+        print("*",end="")
+    print("") 
+
+
+# *
+# **
+# ***
+# ****
+# *****
+# ****
+# ***
+# **
+# *
