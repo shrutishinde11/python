5c4868f38d61c0047ef59ab436e77aa70e486df8
Shruti Shinde <139608230+shrutishinde11@users.noreply.github.com>
2024-02-04 15:44:17 +0530

    Add files via upload
         * 
        * *
       * * *
      * * * *
     * * * * *
    * * * * * *
     * * * * *
      * * * *
       * * *
        * *
         *

---
+++ pattern5.py
@@ -0,0 +1,26 @@
+row=6
+for i in range(1, row+1):
+    for k in range(row-i):
+        print(" " , end ="")
+    for j in range(i):
+        print("*", end=" ")
+    print("")
+for i in range(row-1,0,-1):
+    for k in range(row-i):
+        print(" " , end ="")
+    for j in range(i):
+        print("*", end=" ")
+    print("")
+
+
+#      * 
+#     * *
+#    * * *
+#   * * * *
+#  * * * * *
+# * * * * * *
+#  * * * * *
+#   * * * *
+#    * * *
+#     * *
+#      *
